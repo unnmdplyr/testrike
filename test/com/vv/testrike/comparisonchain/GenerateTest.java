c844d47bff4781e9cd42c0a2274a425b3d9e8a59
Viktor Varallyay <varallyay_viktor@yahoo.com>
2018-07-28 11:59:13 +0200

    inspection added

--- test/com/vv/testrike/comparisonchain/GenerateTest.java
+++ test/com/vv/testrike/comparisonchain/GenerateTest.java
@@ -1,22 +1,45 @@
 package com.vv.testrike.comparisonchain;
 
 import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiField;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.testFramework.LightProjectDescriptor;
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
 
 public class GenerateTest extends LightCodeInsightFixtureTestCase {
+
+    private static class MyDescriptor extends DefaultLightProjectDescriptor {
+        @Override
+        public Sdk getSdk() {
+            return JavaSdk.getInstance().createJdk("1.8", new File(getSourceRoot(), "mockJDK-1.8").getPath(), false);
+        }
+    }
+
+    @NotNull
+    @Override
+    protected LightProjectDescriptor getProjectDescriptor() {
+        return new MyDescriptor();
+    }
+
     @Override
     protected String getTestDataPath() {
+        return new File(getSourceRoot(), "testData").getPath();
+    }
+
+    @NotNull
+    private static File getSourceRoot() {
         String testOutput = PathManager.getJarPathForClass(GenerateTest.class);
-        File sourceRoot = new File(testOutput, "../../..");
-        return new File(sourceRoot, "testData").getPath();
+        return new File(testOutput, "../../..");
     }
 
     public void testSimple() {
@@ -27,5 +50,10 @@ public class GenerateTest extends LightCodeInsightFixtureTestCase {
         new GenerateAction().generateComparable(simpleClass, fields);
 
         myFixture.checkResultByFile("after" + getTestName(false) + ".java");
+    }
+
+    public void testInspection() {
+        myFixture.enableInspections(EqualsCompareConsistencyInspection.class);
+        myFixture.testHighlighting(true, false, false, "inspection.java");
     }
 }
